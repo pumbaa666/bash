0829ac0a25ce1189532f3e3899f7bba207c5cf13
Loic Correvon <loic.correvon@eeproperty.com>
2025-10-27 17:06:35 +0100

    toggle keyboard : - script definition - better usage message

--- toggle_keyboard.sh
+++ toggle_keyboard.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-DEVICE_NAME="AT Translated Set 2 keyboard"
+# Script to enable or disable the keyboard using xinput
+# Usage: ./toggle_keyboard.sh --enable|--disable [device_name]
+# Example: ./toggle_keyboard.sh --disable "AT Translated Set 2 keyboard"
+
+DEVICE_NAME=${2:-"AT Translated Set 2 keyboard"}
 KEYBOARD_ID=$(xinput list | grep "$DEVICE_NAME" | grep -oP 'id=\K[0-9]+')
 
 if [ -z "$KEYBOARD_ID" ]; then
@@ -8,8 +12,11 @@ if [ -z "$KEYBOARD_ID" ]; then
     exit 1
 fi
 
-usage() {
-    echo "Usage: $0 [--enable | -e] | [--disable | -d]"
+function usage() {
+    echo "Usage: $0 [--enable | -e] | [--disable | -d] [device_name]"
+    echo "  --enable, -e    Enable the keyboard"
+    echo "  --disable, -d   Disable the keyboard"
+    echo "  device_name     Optional: Specify a different device name (default: \"${DEVICE_NAME}\")"
     exit 1
 }
 
