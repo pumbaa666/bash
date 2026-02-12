a423b10f210d96d55f309fb523847ce3a8e9b94a
Loic Correvon <loic.correvon@eeproperty.com>
2026-02-12 16:43:17 +0100

    Better install python UX. No need to restart the script twice. (WIP) need some cleaning

--- ebonhold/install-python.bat
+++ ebonhold/install-python.bat
@@ -9,55 +9,67 @@ rem License: MIT License
 
 setlocal
 
+set "PYTHON_VERSION_MAJOR=3.8"
+set "PYTHON_VERSION_FULL=%PYTHON_VERSION_MAJOR%.10"
+
 if /i "%PROCESSOR_ARCHITECTURE%"=="AMD64" (
     set "OS_ARCH=-amd64"
 ) else (
     set "OS_ARCH="
 )
-set "PYTHON_VERSION=3.8.10"
-set "PYTHON_INSTALLER_NAME=python-%PYTHON_VERSION%%OS_ARCH%.exe"
+
+set "PYTHON_INSTALLER_NAME=python-%PYTHON_VERSION_FULL%%OS_ARCH%.exe"
 
 rem Check if python is installed
 python --version >nul 2>&1
 if errorlevel 1 (
     echo Python is not installed or not in PATH.
     if exist "%PYTHON_INSTALLER_NAME%" (
+        echo Found Python installer: %PYTHON_INSTALLER_NAME%. Launching installer...
+        echo Please follow the installer instructions to install Python %PYTHON_VERSION_FULL% and make sure to check the option to add Python to PATH during installation.
+        echo This script will resume after the installer is closed, and will check again for Python and pip before proceeding to install dependencies.
         start /wait "" "%PYTHON_INSTALLER_NAME%"
+
+        rem After installation, try to find the python executable in the registry and set it to PYTHON_EXE variable.
+        rem to avoid reloading PATH or restarting this script and possibly entering an infinite loop.
+        for /f "tokens=2*" %%A in (
+            'reg query "HKLM\Software\Python\PythonCore\%PYTHON_VERSION_MAJOR%\InstallPath" /ve 2^>nul'
+        ) do set "PYTHON_EXE=%%Bpython.exe"
+
+        if not defined PYTHON_EXE (
+            for /f "tokens=2*" %%A in (
+                'reg query "HKCU\Software\Python\PythonCore\%PYTHON_VERSION_MAJOR%\InstallPath" /ve 2^>nul'
+            ) do set "PYTHON_EXE=%%Bpython.exe"
+        )
+
+        if not defined PYTHON_EXE (
+            echo Failed to install or find Python executable. Please install Python %PYTHON_VERSION_FULL% manually from the official website: https://www.python.org/downloads/release/python-%PYTHON_VERSION_FULL%/
+            echo and then restart this script.
+            pause
+            exit /b 1
+        )
     ) else (
         echo Install Python 3.8.10 from the official website: https://www.python.org/downloads/release/python-3810/
         echo Python 3.8.10 is the latest version compatible with Windows 7, but also works on Windows 10/11.
+        pause
         exit /b 1
     )
 ) else (
     echo Python is installed.
 )
 
-rem Check if pip is installed
-python -m pip >nul 2>&1
-if errorlevel 1 (
-    echo pip is not installed or not in PATH.
-    exit /b 1
-) else (
-    echo pip is installed.
-)
-
-rem Check if requests package is installed
-python -c "import requests" >nul 2>&1
-if errorlevel 1 (
-    echo requests package is not installed. Installing it now...
-
+if not exist venv (
     python -m venv venv
-    call venv\Scripts\activate
-    python -m pip install --upgrade pip
-    python -m pip install requests --disable-pip-version-check
-) else (
-    echo requests package is already installed.
+    if errorlevel 1 (
+        echo Error: Failed to create Python virtual environment.
+        pause
+        exit /b 1
+    )
 )
+venv\Scripts\python -m pip install --upgrade pip >nul 2>&1 || exit /b 1
+venv\Scripts\python -m pip install requests --disable-pip-version-check >nul 2>&1 || exit /b 1
+venv\Scripts\python -c "import requests" >nul 2>&1 || exit /b 1
 
-rem Check everything is set up correctly
-python --version >nul 2>&1 || exit /b 1
-python -m pip --version >nul 2>&1 || exit /b 1
-python -c "import requests" >nul 2>&1 || exit /b 1
 echo Python environment is set up correctly. You can now run the ebonhold-launcher.bat to update and launch the game.
 
 endlocal
