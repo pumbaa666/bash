f591bd5758ba85a35d3a4ccad5a4b7e05e5e1435
Loic Correvon <loic.correvon@eeproperty.com>
2026-03-09 11:41:26 +0100

    Fix ipaddress script to work on VPS. Cleanup

--- ipaddress.sh
+++ ipaddress.sh
@@ -1,110 +1,139 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # This program lists all IP addresses of the machine and allows the user to select one to copy to clipboard using arrow keys.
 # Requires xclip to copy to clipboard.
 # If xclip is not installed, it will just print the selected IP address.
 # If -i option is provided, it ignores loopback and IPv6 addresses.
 
-XCLIP_PATH=""
+RED=$'\033[0;31m'
+CYAN=$'\033[0;36m'
+BG_CYAN=$'\033[46m'
+RESET=$'\033[0m'
 
-# Colors
-RED='\033[0;31m'
-BG_RED='\033[41m'
-CYAN='\033[0;36m'
-BG_CYAN='\033[46m'
-GREEN='\033[0;32m'
-RESET='\033[0m'
+XCLIP=""
 
-checkRequiredPrograms() {
-    XCLIP_PATH="$(which xclip)"
-    if [[ -z "${XCLIP_PATH}" ]] ; then
-        echo -e "[WARN] xclip is not installed, the programm won't be able to write to clipboard."
+function check_requirements() {
+    if command -v xclip >/dev/null 2>&1; then
+        XCLIP=$(command -v xclip)
+    else
+        echo -e "[WARN] xclip is not installed, the program won't be able to write to clipboard."
         echo -e "On Debian/Ubuntu, you can install it with:\n    sudo apt install xclip"
     fi
 }
 
-function getIps() {
-    local ignoreUseless=${1:-"false"}
-    if [[ "${ignoreUseless}" == "true" ]]; then
-        ifconfig | grep inet | grep -v "127.0.0.1" | grep -v "::" | grep -Ev "^addr:[ \t]*$" | awk '{print $2}' | sed 's/addr://g'
+function get_ips() {
+    local ignore_useless="${1:-false}"
+
+    if [[ "$ignore_useless" == "true" ]]; then
+        ip -o addr show scope global | awk '{print $4}' | cut -d/ -f1
     else
-        ifconfig | grep inet | grep -Ev "^addr:[ \t]*$" | awk '{print $2}' | sed 's/addr://g'
+        ip -o addr show | awk '{print $4}' | cut -d/ -f1
     fi
 }
 
 # Only exit when called from ctrl-c
-function cleanup() {
+function restore_terminal() {
     local exit_type="$1"
-    tput cnorm
     tput rmcup
+    tput cnorm
+    stty sane
     if [[ "${exit_type}" != "dontexit" ]]; then
         exit 0
     fi
 }
 
-function main() {
-    checkRequiredPrograms
+function enter_terminal() {
+    tput smcup
+    tput civis
+    stty -echo -icanon time 0 min 1
+}
 
-    local ignoreUseless="false"
-    for arg in "$@"; do
-        if [[ "$arg" == "-i" ]]; then
-            ignoreUseless="true"
-            break
+function draw_menu() {
+    local selected="$1"
+    shift
+    local items=("$@")
+
+    tput cup 0 0
+    echo "Select IP (↑/↓ navigate, Enter select, Esc quit)"
+
+    for i in "${!items[@]}"; do
+        if [[ $i -eq $selected ]]; then
+            printf " > %s\n" "${BG_CYAN}${items[$i]}${RESET}"
+        else
+            printf "   %s\n" "${items[$i]}"
         fi
     done
+}
 
-    mapfile -t ips < <(getIps "$ignoreUseless")
-    local selected=0
+function read_key() {
+    local k
+    IFS= read -rsn1 k
 
-    if [[ ${#ips[@]} -eq 0 ]]; then
-        echo "No IP addresses found."
-        exit 0
+    if [[ $k == $'\x1b' ]]; then
+        read -rsn1 -t 0.01 k || { echo ESC; return; }
+        if [[ $k == "[" ]]; then
+            read -rsn1 k
+            case "$k" in
+                A) echo UP ;;
+                B) echo DOWN ;;
+            esac
+        else
+            echo ESC
+        fi
+        return
     fi
 
+    [[ $k == "" ]] && echo ENTER
+}
+
+function copy_ip() {
+    local ip="$1"
+
+    if [[ -n "$XCLIP" ]]; then
+        printf "%s" "$ip" | xclip -selection clipboard
+        echo "Copied ${CYAN}${ip}${RESET} to clipboard"
+    else
+        echo "$ip"
+    fi
+}
+
+function main() {
+    check_requirements
+
+    local ignore_useless=false
+    [[ "${1:-}" == "-i" ]] && ignore_useless=true
+
+    mapfile -t ips < <(get_ips "$ignore_useless")
+
+    [[ ${#ips[@]} -eq 0 ]] && { echo "No IP addresses found."; exit 1; }
+
     local selected=0
-    
-    trap cleanup INT TERM
-    menu_height=$((${#matching_machines[@]} + 1))
-    tput smcup # Enter alternate screen
-    tput civis # Hide cursor
+
+    trap restore_terminal INT TERM
+    enter_terminal
 
     while true; do
-        tput cup 0 0   # move cursor to top-left
-        echo "Select an IP address (UP/DOWN to navigate, ENTER to copy, ESC to quit):"
-        for i in "${!ips[@]}"; do
-            if [[ $i -eq $selected ]]; then
-                echo -e "${BG_CYAN} > ${ips[$i]}${RESET}"
-            else
-                echo "   ${ips[$i]}"
-            fi
-        done
-        tput el # clear leftovers if menu shrank
+        draw_menu "$selected" "${ips[@]}"
 
-        # Read user input
-        read -rsn1 key
-        if [[ "$key" == $'' ]]; then
-            read -rsn1 -t 0.1 key
-            if [[ "$key" == "[" ]]; then
-                read -rsn1 key
-                if [[ "$key" == "A" ]]; then # Up
-                    selected=$(( (selected - 1 + ${#ips[@]}) % ${#ips[@]} ))
-                elif [[ "$key" == "B" ]]; then # Down
-                    selected=$(( (selected + 1) % ${#ips[@]} ))
-                fi
-            else # Escape
-                cleanup "dontexit"
-                return 1
-            fi
-        elif [[ "$key" == "" ]]; then # Enter
-	        cleanup "dontexit"
-            if [[ -n "${XCLIP_PATH}" ]]; then
-                echo -n "${ips[$selected]}" | xclip -selection clipboard
-                echo -e "Copied ${CYAN}${ips[$selected]}${RESET} to clipboard."
-            else
-                echo -e "${ips[$selected]}"
-            fi
-            return 0
-        fi
+        case "$(read_key)" in
+            UP)
+                ((selected--))
+                ((selected<0)) && selected=$((${#ips[@]}-1))
+                ;;
+            DOWN)
+                ((selected++))
+                ((selected>=${#ips[@]})) && selected=0
+                ;;
+            ENTER)
+                restore_terminal "dontexit"
+                copy_ip "${ips[$selected]}"
+                exit 0
+                ;;
+            ESC)
+                restore_terminal "dontexit"
+                exit 1
+                ;;
+        esac
     done
 }
 
